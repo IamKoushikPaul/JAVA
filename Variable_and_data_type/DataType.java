992ffa6767c57f31cbbfc78a5723f2e59b047e7d
Koushik <koushik2005paul@gmail.com>
2026-09-24 12:36:03 +0600

    1st

---
+++ Variable_and_data_type/DataType.java
@@ -0,0 +1,25 @@
+package Variable_and_data_type;
+
+public class DataType {
+    public static void main(String[] args) {
+        boolean b = true; //dynamic initialization
+        char c = 'A';
+        short s = 100;
+        int i = 1000;
+        double d = 10.5;
+        float f= 10.4f;
+      /*b = true;
+        c = 'A';
+        s = 100;
+        i = 1000;
+        d = 10.5;*/
+
+        System.out.println("boolean b = "+b);
+        System.out.println("char c = "+c);
+        System.out.println("short s = "+s);
+        System.out.println("int i = "+i);
+        System.out.println("double d = "+d);
+        System.out.println("float f = "+f);
+        
+    }
+}
