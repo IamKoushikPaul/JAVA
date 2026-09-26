74da6a4be9f7e4f85541487300546f268567af4e
Koushik <koushik2005paul@gmail.com>
2026-09-27 00:18:45 +0600

    1st

---
+++ AnisulIslam/Assignment_Operator.java
@@ -0,0 +1,19 @@
+package AnisulIslam;
+
+public class Assignment_Operator {
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 4;
+
+        x+=y;
+        System.out.println("X = "+x);
+        x-=y;
+        System.out.println("X = "+x);
+        x*=y;
+        System.out.println("X = "+x);
+        x/=y;
+        System.out.println("X = "+x);
+        x%=y;
+        System.out.println("X = "+x);
+    }
+}
